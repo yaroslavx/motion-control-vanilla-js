e708c6bc1a55b325eb2c3e025bc6a0da5aeff922
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-12-13 19:30:34 +0300

    add hand tracking

---
+++ js/track-hand-motions.js
@@ -0,0 +1,3 @@
+import { Camera } from '@mediapipe/camera_utils';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
+import { Hands, HAND_CONNECTIONS } from '@mediapipe/hands';
